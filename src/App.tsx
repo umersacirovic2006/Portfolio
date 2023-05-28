0b9b2e44191b0939e8b02b89fa8e86430596489d
umersacirovic2006 <asacirovic90@gmail.com>
2023-05-28 13:05:11 +0200

    Currently under construction

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import './App.css'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <h1>Currently under construction.</h1>
+    </div>
   )
 }
 
