4f71a6b1a9331f4556c6b7e772e2c4223d2fa841
umersacirovic2006 <asacirovic90@gmail.com>
2023-05-29 21:02:11 +0200

    More pages and working on the first release

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Demo from './pages/Demo/Demo'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
 
 function App() {
   return (
     <div className='App'>
-      <h1>Currently under construction.</h1>
+      <Routes>
+        <Route path='/' element={<Demo />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   )
 }
